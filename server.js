fd109a43577e0f5e638577143c918cc95433a081
unknown <amehi4yus@gmail.com>
2021-12-19 16:23:42 -0800

    Prepare for production

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/exercises', require('./routes/exercises'))
 app.use('/api/auth', require('./routes/auth'))
 
+//SERVE STATIC ASSETS IN PRODUCTION?
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {  
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) //
+    })
+}
+
 //Listen to server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log (`Server is running on port ${PORT}`))
